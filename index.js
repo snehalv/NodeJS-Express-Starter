45973ba37cbd41d4d14be6c6e87d4bf93db04417
snehalv <snehalv.2010@gmail.com>
2024-09-25 18:11:10 +0530

    Basic Routes for products and orders

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require('express');
-// const { resolve } = require('path');
 const app = express();
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It Works!'
-  });
-});
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+// For Initial App
+// app.use((req, res, next) => {
+//   res.status(200).json({
+//     message: 'It Works!'
+//   });
+// });
 module.exports = app;
 
+
+// const { resolve } = require('path');
 // const port = 3010;
-
 // app.use(express.static('static'));
-
 // app.get('/', (req, res) => {
 //   res.sendFile(resolve(__dirname, 'pages/index.html'));
 // });
-
 // app.listen(port, () => {
 //   console.log(`Example app listening at http://localhost:${port}`);
 // });
